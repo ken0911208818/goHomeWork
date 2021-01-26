35e313de763d0b1da583d66a140978208d369292
ken <u1045235@hk.edu.tw>
2021-01-26 14:13:23 +0800

    add router about role

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ken0911208818/goHomeWork/handler"
+)
+
+func main() {
+	r := gin.Default()
+	role := r.Group("/role")
+	{
+		role.GET("/", handler.Index)
+		role.POST("/", handler.Create)
+		role.GET("/:id", handler.GetOne)
+		role.PUT("/:id", handler.Update)
+		role.DELETE("/:id", handler.Delete)
+	}
+	r.Run(":8080")
+}
